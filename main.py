95b8bca449347f1636ce7c56228f136a01bafe15
Nearland <tima-makarov-1995@mail.ru>
2021-03-21 20:00:19 +0300

    news bot

--- main.py
+++ main.py
@@ -2,13 +2,69 @@ import feedparser
 import pyowm
 from pyowm.exceptions import api_response_error, api_call_error
 import telebot
+from selenium import webdriver
+from time import sleep
 
 bot = telebot.TeleBot("1644586994:AAGtW78FVpmDscoiV-ZRWAXNvFLrJ-aKjbo")  # апи бота
 
 owm = pyowm.OWM('797385fc66158e63cb61ac82a7d4ee8c', language="ru")  # ключ и язык
 
+#driver = webdriver.Edge('C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe')
 
-# @bot.message_handler(content_types=['text'])  # отвечает на тип текст
+
+#@bot.message_handler(commands=['search_in_youtube'])
+#def search_youtube(message):
+  #  messages = bot.send_message(message.chat.id, "Введите текст, который хотите найти на YouTube?")
+  #  bot.register_next_step_handler(messages, search)  # после вызова команды search_in_youtube идет это команда
+
+
+#@bot.message_handler(content_types='text')
+#def text(message):
+  #  pass
+
+
+#def search(message):
+ #   bot.send_message(message.chat.id, "Начинаю поиск")
+  #  link = "https://www.youtube.com/results?search_query=" + message.text  # ссылка на поиск видео в YouTube
+   # driver.get(link)
+   # video = driver.find_element_by_id("video-title")  # элементы на ютую видео
+   # for i in range(len(video)):
+   #     bot.send_message(message.chat.id, video[i].get_attribute('href'))
+
+
+@bot.message_handler(commands=['news'])
+def news(message):
+    def parseRSS(rss_url):
+        return feedparser.parse(rss_url)
+
+    def getHeadlines(rss_url):
+        headlines = []
+
+        feed = parseRSS(rss_url)
+        for newsitem in feed['items']:
+            # headlines.append(newsitem['title'])
+            headlines.append(newsitem['link'])
+            # headlines.append(newsitem['id'])
+            # headlines.append(newsitem['summary'])
+            # headlines.append(newsitem['published'])
+
+        return headlines
+
+    allheadlines = []
+
+    newsurls = {
+        'googlenews': 'https://news.google.com/rss?hl=ru&gl=RU&ceid=RU:ru',  # сайт с новостями
+    }
+
+    for key, url in newsurls.items():
+        allheadlines.extend(getHeadlines(url))
+
+    for hl in allheadlines:
+        pass
+        bot.send_message(message.chat.id, hl)
+
+
+@bot.message_handler(commands=['weather'])
 def weather(message):  # дублирует
     try:
         city_name = message.text[9:]  # цифра 9 удаляет слово weather
@@ -29,47 +85,6 @@ def weather(message):  # дублирует
         if city_name == "":
             bot.send_message(message.chat.id, "Вы не ввели город/страна")
             pass
-
-
-def news(message):
-    def parseRSS(rss_url):
-        return feedparser.parse(rss_url)
-
-    def getHeadlines(rss_url):
-        headlines = []
-
-        feed = parseRSS(rss_url)
-        for newsitem in feed['items']:
-            #headlines.append(newsitem['title'])
-            headlines.append(newsitem['link'])
-            # headlines.append(newsitem['id'])
-            # headlines.append(newsitem['summary'])
-            # headlines.append(newsitem['published'])
-
-        return headlines
-
-    allheadlines = []
-
-    newsurls = {
-        'googlenews': 'https://news.google.com/rss?hl=ru&gl=RU&ceid=RU:ru',
-    }
-
-    for key, url in newsurls.items():
-        allheadlines.extend(getHeadlines(url))
-
-    for hl in allheadlines:
-        pass
-        bot.send_message(message.chat.id, hl)
-
-
-@bot.message_handler(commands=['news'])
-def command2(message):
-    news(message)
-
-
-@bot.message_handler(commands=['weather'])
-def command1(message):
-    weather(message)
 
 
 bot.polling(none_stop=True)
